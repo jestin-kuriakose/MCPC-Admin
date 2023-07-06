74fec328fe31e3ea3df3b0d6e9d16beb05c57f7d
jestin-kuriakose <jestin@teslaelectronics.ca>
2023-07-07 00:35:20 +0400

    added postgresql

--- api/config/db.js
+++ api/config/db.js
@@ -1,11 +1,12 @@
 import { Sequelize } from "sequelize";
 
 // Create a new Sequelize instance
-const sequelize = new Sequelize('peheabmy_mcpc-admin', 'peheabmy_fsd_admin', 'Kitchener121!', {
-    host: '96.125.174.19',
-    dialect: 'mysql',
-  });
+// const sequelize = new Sequelize('peheabmy_mcpc-admin', 'peheabmy_fsd_admin', 'Kitchener121!', {
+//     host: '96.125.174.19',
+//     dialect: 'mysql',
+//   });
 
+const sequelize = new Sequelize('postgres://fsd_admin:1QPo1FhVQoAMlaYHBDtzTyKuS0MzEKdN@dpg-cijd1jlgkuvhnnedcdtg-a.oregon-postgres.render.com/mcpc_admin_db')
 
 // Test the database connection
 sequelize
